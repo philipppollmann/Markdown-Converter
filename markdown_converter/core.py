aeb3743ca400c9a7959b792b967e44a8179c3273
Philipp Pollmann <philipp@geuer-pollmann.de>
2024-12-18 22:53:45 +0100

    feat: add core convert logic

--- markdown_converter/core.py
+++ markdown_converter/core.py
@@ -4,3 +4,13 @@ This module is the core module of the markdown_converter package.
 from markitdown import MarkItDown
 
 converter = MarkItDown()
+
+def convert_to_markdown(input_file: str) -> str:
+    """
+    Convert the input file to markdown.
+
+    Args: input_file: The input file to convert.
+    Returns: The markdown representation of the input file.
+    """
+
+    return converter.convert(input_file).text_content
